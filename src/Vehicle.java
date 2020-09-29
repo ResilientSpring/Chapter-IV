d44975d8d94237936430915a2cde05e0123e5e44
ted10014 <ted10014@gmail.com>
2020-09-29 23:59:54 +0800

    Committed 2020/09/29

---
+++ src/Vehicle.java
@@ -0,0 +1,13 @@
+
+class Vehicle {
+	
+	int passengers; // number of passengers.
+	int fuelcap;    // fuel capacity in gallons.
+	int mpg;        // fuel consumption in miles per gallon.
+	
+	// Display the range.
+	void range() {    // The range() method is contained within the Vehicle class.
+		System.out.println("Range is " + fuelcap * mpg);
+	}
+
+}
